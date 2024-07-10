8739ba35714edea2cc9a690a86d3e7b7f8256743
nirmalalla <allanirmal@gmail.com>
2024-07-10 17:53:51 -0500

    trying new import

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { Container, Title, Avatar } from "@mantine/core";
+import profileIcon from "../assets/ProfileIcon.png"
 
 function Header() {
     const scrollToContent = () => {
@@ -11,7 +12,7 @@ function Header() {
     return (
         <div style={{ height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', textAlign: 'center', backgroundColor: '#242424' }}>
             <Container style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                <Avatar src="personal-website\src\assets\ProfileIcon.png" size={120} style={{ marginBottom: 20 }} />
+                <Avatar src={profileIcon} size={120} style={{ marginBottom: 20 }} />
                 <Title order={1} style={{ color: '#fff' }}>Hello! I'm Nirmal, nice to meet you.</Title>
             </Container>
             <div style={{ marginTop: 20 }}>
